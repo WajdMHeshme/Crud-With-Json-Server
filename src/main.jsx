3d26e3a4133bd754e6a0491aec2d50a648dc81b3
unknown <wajdtitos@gmail.com>
2025-05-28 13:59:44 +0300

    fix prob

--- src/main.jsx
+++ src/main.jsx
@@ -1,90 +1,39 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import "./index.css";
 import Edit from "./pages/Edit.jsx";
-import Route from "./Route.jsx";
 import Home from "./pages/Home/Home.jsx";
-const routes = createBrowserRouter([
+import Layout from "./Route.jsx"; // تأكد أنه يحتوي على <Outlet />
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "edit/:id",
+          element: <Edit />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Route />,
-    children: [
-      {
-        index: true,
-        path: "",
-        element: <Home />,
-      },
-       {
-         path: "edit/:id",
-         element: <Edit />,
-       },
-    ],
-  },
-]
+    basename: "/Crud-With-Json-Server",
+  }
 );
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={routes} />
+    <RouterProvider router={router} />
   </StrictMode>
 );
-
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Route from "./Route.tsx";
-// import Home from "./pages/Home.tsx";
-// import About from "./pages/About.tsx";
-// import Service from "./pages/Service.tsx";
-// import NewProp from "./pages/NewProp.tsx";
-// import Contact from "./pages/Contact.tsx";
-// import "./index.css";
-// import AOS from "aos";
-// import "aos/dist/aos.css";
-
-// AOS.init({
-//   duration: 1500,
-//   once: true,
-// });
-
-// const routes = createBrowserRouter(
-//   [
-//     {
-//       path: "/",
-//       element: <Route />,
-//       children: [
-//         {
-//           index: true,
-//           path: "",
-//           element: <Home />,
-//         },
-//         {
-//           path: "about",
-//           element: <About />,
-//         },
-//         {
-//           path: "service",
-//           element: <Service />,
-//         },
-//         {
-//           path: "property",
-//           element: <NewProp />,
-//         },
-//         {
-//           path: "contact",
-//           element: <Contact />,
-//         },
-//       ],
-//     },
-//   ],
-//   {
-//     basename: "/Task4_Adv_Flora",
-//   }
-// );
-
-// createRoot(document.getElementById("root")!).render(
-//   <StrictMode>
-//     <RouterProvider router={routes} />
-//   </StrictMode>
-// );
